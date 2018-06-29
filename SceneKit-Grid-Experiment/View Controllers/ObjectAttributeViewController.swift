8835d385ccfbe2a0cc5fe1d23f13d2cffaae2d5f
Trevin Wisaksana <trevinwisaksana@gmail.com>
2018-06-29 14:27:05 +0700

    Fixed collection view auto layout in Popover Menu

---
+++ SceneKit-Grid-Experiment/View Controllers/ObjectAttributeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ObjectAttributeViewController.swift
+//  SceneKit-Grid-Experiment
+//
+//  Created by Trevin Wisaksana on 29/06/2018.
+//  Copyright © 2018 Trevin Wisaksana. All rights reserved.
+//
+
+import UIKit
+
+final class ObjectAttributeViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+}
