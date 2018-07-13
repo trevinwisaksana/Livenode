b993992540f517b04a309f7abadb6c42a421de75
Trevin Wisaksana <trevinwisaksana@gmail.com>
2018-07-13 16:19:22 +0700

    Added play button in the Navigation Bar

--- SceneKit-Grid-Experiment/View Controllers/ObjectCatalogViewController.swift
+++ SceneKit-Grid-Experiment/View Controllers/ObjectCatalogViewController.swift
@@ -80,7 +80,7 @@ extension ObjectCatalogViewController: UICollectionViewDelegateFlowLayout {
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
 
-        let width = view.frame.width / 2
+        let width = view.frame.width / 2.1
 
         return CGSize(width: width, height: width)
     }
