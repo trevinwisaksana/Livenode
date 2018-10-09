60a6236125cde76c83786e82888e65e924a2ed62
Trevin Wisaksana <trevinwisaksana@gmail.com>
2018-10-09 21:55:39 +0700

    Removed unecessary node highlight code

--- SceneKit-Grid-Experiment/Extensions/SceneKit/SCNodeExtensions.swift
+++ SceneKit-Grid-Experiment/Extensions/SceneKit/SCNodeExtensions.swift
@@ -30,26 +30,6 @@ extension SCNNode {
         }
     }
     
-    // MARK: - Highlight
-    
-    private struct HighlightState {
-        static var isHighlighted = false
-    }
-    
-    var isHighlighted: Bool {
-        get {
-            guard let highlightState = objc_getAssociatedObject(self, &HighlightState.isHighlighted) as? Bool else {
-                return Bool()
-            }
-            
-            return highlightState
-        }
-        
-        set(value) {
-            objc_setAssociatedObject(self, &HighlightState.isHighlighted, value, objc_AssociationPolicy.OBJC_ASSOCIATION_RETAIN_NONATOMIC)
-        }
-    }
-    
 }
 
 extension SCNNode: NodeInspectorViewModel {
