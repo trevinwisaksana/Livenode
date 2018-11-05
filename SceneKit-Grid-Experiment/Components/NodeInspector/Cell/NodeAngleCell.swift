c56620d329b643ddcbea155970c5d10e52cbe723
Trevin Wisaksana <trevinwisaksana@gmail.com>
2018-11-05 21:01:48 +0700

    Changed size of plusMinusSegmentedControl width

--- SceneKit-Grid-Experiment/Components/NodeInspector/Cell/NodeAngleCell.swift
+++ SceneKit-Grid-Experiment/Components/NodeInspector/Cell/NodeAngleCell.swift
@@ -25,7 +25,7 @@ public class NodeAngleCell: UITableViewCell {
     private static let angleTextFieldlRightMargin: CGFloat = -15.0
     private static let angleTextFieldlWidth: CGFloat = 70.0
     
-    private static let plusMinusSegmentedControlWidth: CGFloat = 100.0
+    private static let plusMinusSegmentedControlWidth: CGFloat = 80.0
     private static let plusMinusSegmentedControlRightMargin: CGFloat = -15.0
     
     private var currentAngleValue: Int = 0
