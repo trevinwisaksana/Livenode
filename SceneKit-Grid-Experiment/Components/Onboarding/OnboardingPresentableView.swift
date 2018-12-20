eaa8a08707fe722b876c8bb774c9d3a09d733339
Trevin Wisaksana <trevinwisaksana@gmail.com>
2018-12-20 10:52:23 +0700

    Removed onboarding image view animation

--- SceneKit-Grid-Experiment/Components/Onboarding/OnboardingPresentableView.swift
+++ SceneKit-Grid-Experiment/Components/Onboarding/OnboardingPresentableView.swift
@@ -84,36 +84,8 @@ final class OnboardingPresentableView: UIView {
     }
     
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        // TODO: Fix issue with the sizing of the instruction image view
         let pageIndex = round(scrollView.contentOffset.x / frame.width)
         pageControl.currentPage = Int(pageIndex)
-        
-        let maximumHorizontalOffset: CGFloat = scrollView.contentSize.width - scrollView.frame.width
-        let currentHorizontalOffset: CGFloat = scrollView.contentOffset.x
-        
-        let maximumVerticalOffset: CGFloat = scrollView.contentSize.height - scrollView.frame.height
-        let currentVerticalOffset: CGFloat = scrollView.contentOffset.y
-        
-        let percentageHorizontalOffset: CGFloat = currentHorizontalOffset / maximumHorizontalOffset
-        let percentageVerticalOffset: CGFloat = currentVerticalOffset / maximumVerticalOffset
-    
-        let percentOffset: CGPoint = CGPoint(x: percentageHorizontalOffset, y: percentageVerticalOffset)
-        
-        if(percentOffset.x > 0 && percentOffset.x <= 0.25) {
-            pages[0].instructionImageView.transform = CGAffineTransform(scaleX: (0.25 - percentOffset.x) / 0.25, y: (0.25 - percentOffset.x) / 0.25)
-            pages[1].instructionImageView.transform = CGAffineTransform(scaleX: percentOffset.x / 0.25, y: percentOffset.x / 0.25)
-            
-        } else if(percentOffset.x > 0.25 && percentOffset.x <= 0.50) {
-//            pages[1].instructionImageView.transform = CGAffineTransform(scaleX: (0.50 - percentOffset.x) / 0.25, y: (0.50 - percentOffset.x) / 0.25)
-            pages[2].instructionImageView.transform = CGAffineTransform(scaleX: percentOffset.x / 0.50, y: percentOffset.x / 0.50)
-            
-        } else if(percentOffset.x > 0.50 && percentOffset.x <= 0.75) {
-            pages[2].instructionImageView.transform = CGAffineTransform(scaleX: (0.75 - percentOffset.x) / 0.25, y: (0.75 - percentOffset.x) / 0.25)
-            pages[3].instructionImageView.transform = CGAffineTransform(scaleX: percentOffset.x / 0.75, y: percentOffset.x / 0.75)
-            
-        } else if(percentOffset.x > 0.75 && percentOffset.x <= 1) {
-            pages[3].instructionImageView.transform = CGAffineTransform(scaleX: (1-percentOffset.x)/0.25, y: (1-percentOffset.x)/0.25)
-        }
     }
     
 }
